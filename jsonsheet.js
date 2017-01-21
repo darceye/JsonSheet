3809628e0bab3002d761d54e7c7376141da45c6c
yang <swandarc@yandex.com>
2017-01-21 23:34:26 +0800

    Add multi-line titles support

--- jsonsheet.js
+++ jsonsheet.js
@@ -49,18 +49,129 @@ function JsonSheet(data, sum){
         this.sum = []
     }
 }
+/*
+找当前key位置前面不为空的Key. 
+isReturnC为true的时候, 返回BeforeKey的c位置.
+*/
+function findBeforeKey(array, data, currentLocation, isReturnC){
+    var beforeKey;
+    var findC;
+    for(findC = currentLocation.c - 1; findC >= data.s.c; findC--){
+        beforeKey = array[currentLocation.r - 1][findC - 1]
+        if(beforeKey != undefined && beforeKey != ""){
+            break;
+        }
+    }
+    if(typeof(isReturnC) != "undefined" && isReturnC){
+        return {
+            c: findC,
+            key: beforeKey
+        }
+    }else{
+        return beforeKey
+    }
+   
+}
+// function findKeyOnBeforeKey(array, data, currentLocation){
+//     var keyOnBeforeKey;
+//     var beforeKey;
+//     for(var findC = currentLocation.c - 1; findC >= data.e.c; findC--){
+//         beforeKey = array[currentLocation.r - 1][findC - 1]
+//         if(beforeKey != undefined){
+//             // 
+//             if()
+//             break;
+//         }
+//     }
+
+// }
+function title2KeyChains(array, data){
+
+    var keyChains = []
+    if(array[data.s.r - 1][data.s.c - 1] == undefined){
+        console.warn("The first cell is empty, canceled.")
+        return []
+    }
+
+    /*
+    Generate key-chain
+    方式: 当前key为空, 上一行也空(第一个key), 则向同一行前面找不为空的作为当前key
+    
+    方式: 当前key为空, 上一行不为空, 则: 
+    需要区分如下的emptyA和emptyB, 
+    emptyA向前找到b2, 从b2向上找到b, 从当前位置向上找到start, b != start, 则 emptyA为空.
+    emptyB向前找到end, 从end向上找到duration, 从当前位置向上找到duration, 两者相等, 
+    则 emptyB为end.
+
+    type            duration        
+    a       b       start   end     *emptyB
+    a1      b2      *emptyA end1    end2
+    */
+    for(var c = data.s.c ; c <= data.e.c; c++){
+        var keyChain = []
+        for(var r = data.s.r; r <= data.e.r; r++){
+            var key = array[r - 1 ][c - 1]
+            if(key != undefined && key != ""){
+                keyChain.push(key)
+            }else if(keyChain.length == 0){
+                var beforeKey = findBeforeKey(array, data, {c: c, r: r})    
+                console.assert(beforeKey != undefined, "Cannot find beforeKey")
+
+                keyChain.push(beforeKey)
+            }else{
+                // 区分emptyA和emptyB
+                var before = findBeforeKey(array, data, {c: c, r: r}, true)
+
+                //向前未找到, 则认为属于EmptyA的情况.
+                if(before.key != undefined && before.key != ""){
+                    console.assert(r >=2, "不应该是第一行")
+                    var onBeforeKey = keyChains[before.c - data.s.c][r - 2]
+                    var onThisKey = keyChain[r - 2]
+
+                    if(onBeforeKey == onThisKey){
+                        keyChain.push(before.key)
+                    }
+                }
+            }
+        }
+        keyChains.push(keyChain)
+    }
+    return keyChains
+}
 
 function array2Json(array,  data, titleLineNum, sumLineNum){
     var json = []
+    var keyChains = title2KeyChains(array, 
+        {
+            s:{r: data.s.r, c: data.s.c}, 
+            e:{r: data.s.r + titleLineNum - 1, c: data.e.c}
+        })
+
+    console.log("keyChains",keyChains)
+
     for( var r = data.s.r + titleLineNum; r <= data.e.r; r++){
-            var onedata = {}
+        var onedata = {}
         for(var c = data.s.c ; c <= data.e.c; c++){
-            var key = array[data.s.r - 1][c - 1]
-            onedata[key] = array[r - 1][c - 1]
+            // var key = array[data.s.r - 1][c - 1]
+            // onedata[key] = array[r - 1][c - 1]
+            assignKeyChain(onedata, keyChains[c - data.s.c ], array[r - 1][c - 1])
         }
         json.push(onedata)
     }
     return json
+}
+
+function assignKeyChain(obj, keyChain, value){
+    var key = keyChain[0]
+    if(keyChain.length === 1){
+        obj[key] = value
+    } else{
+        if(!obj.hasOwnProperty(key)){
+            obj[key] = {}
+
+        }
+        assignKeyChain(obj[key], keyChain.slice(1), value)
+    }
 }
 
 window.wb = wb
@@ -70,3 +181,7 @@ window.Utils = Utils
 module.exports = JsonSheet
 var ex = module.exports
 ex.array2Json = array2Json
+
+ex.assignKeyChain = assignKeyChain
+ex.title2KeyChains = title2KeyChains
+ex.findBeforeKey = findBeforeKey
--- test/test.js
+++ test/test.js
@@ -1,6 +1,117 @@
 var expect = require('chai').expect
 var jsonsheet  =require('../jsonsheet')
 
+
+describe('assign key chain', function(){
+	it('one key', function(){
+		var obj = {}
+		var kc = ['type']
+		var value = "fight"
+		jsonsheet.assignKeyChain(obj, kc ,value)
+		expect(obj).to.be.deep.equal({type: 'fight'})
+	})
+	it('keys', function(){
+		var obj = {}
+		var kc = ['type','subtype','basetype']
+		var value = "fight"
+		jsonsheet.assignKeyChain(obj, kc ,value)
+		expect(obj).to.be.deep.equal({type: {subtype: {basetype: 'fight'}}})
+	})
+})
+
+describe('title 2 keychain', function () {
+	it('1 title', function(){
+		var array = [
+			['id','duration'],
+			['','start','end'],
+			[1,'09:00','18:00'],
+			[2,'10:00', "19:00"]
+		]
+		var data =  {s:{c:1, r:1}, e:{c:3, r:1}}
+
+		var keychain = jsonsheet.title2KeyChains(array, data)
+		expect(keychain).to.be.deep.equal([['id'],['duration'],['duration']])
+	})
+	it('2 titles', function(){
+		var array = [
+			['id','duration'],
+			['','start','end'],
+			[1,'09:00','18:00'],
+			[2,'10:00', "19:00"]
+		]
+		var data =  {s:{c:1, r:1}, e:{c:3, r:2}}
+
+		var keychain = jsonsheet.title2KeyChains(array, data)
+		console.log(JSON.stringify(keychain))
+		expect(keychain).to.be.deep.equal([
+			['id'],
+			['duration','start'],
+			['duration','end']])
+	})
+	it('3 titles', function(){
+		var array = [
+			['id'	,'duration'	,		,		,'name'],
+			[''		,'start'	,'end'			],
+			[		,			,'end1'	,'end2'	],
+			[1,'09:00','18:00'],
+			[2,'10:00', "19:00"]
+		]
+		var data =  {s:{c:1, r:1}, e:{c:5, r:3}}
+
+		var keychain = jsonsheet.title2KeyChains(array, data)
+		console.log(JSON.stringify(keychain))
+		expect(keychain).to.be.deep.equal([
+			['id'],
+			['duration','start'],
+			['duration','end','end1'],
+			['duration','end','end2'],
+			['name']])
+	})
+})
+
+
+describe("findBeforeKey", function(){
+	it('findBeforeKey', function () {
+		var array = [
+			['id'	,'duration'	,		,		,'name'],
+			[''		,'start'	,'end'			],
+			[		,			,'end1'	,'end2'	],
+			[1,'09:00','18:00'],
+			[2,'10:00', "19:00"]
+		]
+		var data =  {s:{c:1, r:1}, e:{c:5, r:3}}
+		var beforeKey = jsonsheet.findBeforeKey(array, data, {c:4,r:2})
+		expect(beforeKey).to.be.equal('end')
+	})
+	it('findBeforeKey and C', function () {
+		var array = [
+			['id'	,'duration'	,		,		,'name'],
+			[''		,'start'	,'end'			],
+			[		,			,'end1'	,'end2'	],
+			[1,'09:00','18:00'],
+			[2,'10:00', "19:00"]
+		]
+		var data =  {s:{c:1, r:1}, e:{c:5, r:3}}
+		var before = jsonsheet.findBeforeKey(array, data, {c:4,r:2}, true)
+		expect(before.key).to.be.equal('end')
+		expect(before.c).to.be.equal(3)
+	})
+	it('findBeforeKey and C2', function () {
+		var array = [
+			['id'	,'duration'	,		,		,'name'],
+			[''		,'start'	,'end'			],
+			[		,			,'end1'	,'end2'	],
+			[1,'09:00','18:00'],
+			[2,'10:00', "19:00"]
+		]
+		var data =  {s:{c:1, r:1}, e:{c:5, r:3}}
+		var before = jsonsheet.findBeforeKey(array, data, {c:4,r:1}, true)
+		expect(before.key).to.be.equal('duration')
+		expect(before.c).to.be.equal(2)
+	})
+})
+
+
 describe('array 2 json', function(){
 
 	it('one line title ,one line data', function(){
@@ -20,7 +131,7 @@ describe('array 2 json', function(){
 			{id: 1, name:'yang'},{id:2, name:'qiang'}])
 	})
 
-	it('one line title ,one line data2', function(){
+	it('one line title ,one line data3', function(){
 		var array = [['id','name'],[1,'yang'],[2,'qiang']]
 		var data = {s:{c:2, r:1}, e:{c:2, r:3}}
 		var json = jsonsheet.array2Json(array, data,1)
@@ -28,7 +139,7 @@ describe('array 2 json', function(){
 		expect(json).to.be.deep.equal([
 			{name:'yang'},{name:'qiang'}])
 	})
-	it('one line title ,one line data2', function(){
+	it('one line title ,one line data4', function(){
 		var array = [['id','name'],[1,'yang'],[2,'qiang']]
 		var data = {s:{c:2, r:1}, e:{c:2, r:2}}
 		var json = jsonsheet.array2Json(array, data,1)
@@ -37,18 +148,39 @@ describe('array 2 json', function(){
 			{name:'yang'}])
 	})
 	
-	it('one line title ,one line data2', function(){
+	it('2 line titles ,2 line data', function(){
 		var array = [
 			['id','duration'],
 			['','start','end'],
 			[1,'09:00','18:00'],
 			[2,'10:00', "19:00"]
 		]
-		var data = {s:{c:2, r:1}, e:{c:2, r:2}}
-		var json = jsonsheet.array2Json(array, data,1)
+		var data = {s:{c:1, r:1}, e:{c:3, r:4}}
+		var json = jsonsheet.array2Json(array, data,2)
 		console.log(json)
 		expect(json).to.be.deep.equal([
-			{name:'yang'}])
+			{id:1, duration:{start: '09:00', end: '18:00'}},
+			{id:2, duration:{start: '10:00', end: '19:00'}},	
+		])
 	})
 	
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
